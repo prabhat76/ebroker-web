a6b718cdf150a4b88fd2642fd85581331ef61ca7
Prabhat kumar <prabhatkumar@Prabhats-MacBook-Pro.local>
2025-11-28 22:50:44 +0530

    vercel chnages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-const fs = require("fs");
 
 const nextConfig = {
   reactStrictMode: false,
@@ -24,20 +22,5 @@ const nextConfig = {
     buildActivity: false,
   },
 };
-if (process.env.NEXT_PUBLIC_SEO === "false") {
-  nextConfig.output = "export";
-  nextConfig.exportPathMap = async (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId },
-  ) => {
-    if (dir && outDir && fs.existsSync(path.join(dir, ".htaccess"))) {
-      fs.copyFileSync(
-        path.join(dir, ".htaccess"),
-        path.join(outDir, ".htaccess"),
-      );
-    } else {
-    }
-    return defaultPathMap;
-  };
-}
+
 module.exports = nextConfig;
